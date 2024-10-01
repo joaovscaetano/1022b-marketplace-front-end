a1f720cccefcd7c5c7e35f0cb51c963c45674b97
João Caetano <118355857+joaovscaetano@users.noreply.github.com>
2024-10-01 08:30:23 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-useEffect(()=>{
-  fetch("https://one022b-marketplace-ltd9.onrender.com/produtos")
-  .then(resposta=>resposta.json())
-  .then(data=>console.log(data))
-  console.log("Executei o useEffect")
-  
-},[])
+  const [produtos, setProdutos] = useState()
+
+  useEffect(() => {
+    fetch("https://one022b-marketplace-ltd9.onrender.com/produtos")
+      .then(resposta => resposta.json())
+      .then(data => console.log(data))
+  }, [])
   return (
     <>
+      {
+        console.log(produtos)
+      }
     </>
   )
 }
